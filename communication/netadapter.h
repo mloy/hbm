fe80705138c1750d268669cd64f388ec24af4077
Matthias Loy <matthias.loy@hbm.com>
2014-09-16 07:35:12 +0000

    move generic communication classes
    git-svn-id: http://repository.devel.hbm.com/svn/quantumx/trunk@21082 7cbc33cc-6077-418a-a044-ec03dd7e8bdb

---
+++ communication/netadapter.h
@@ -0,0 +1,139 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: t; c-basic-offset: 4 -*- */
+/*
+ * Copyright (C) 2007 Hottinger Baldwin Messtechnik GmbH
+ * Im Tiefen See 45
+ * 64293 Darmstadt
+ * Germany
+ * http://www.hbm.com
+ * All rights reserved
+ *
+ * The copyright to the computer program(s) herein is the property of
+ * Hottinger Baldwin Messtechnik GmbH (HBM), Germany. The program(s)
+ * may be used and/or copied only with the written permission of HBM
+ * or in accordance with the terms and conditions stipulated in the
+ * agreement/contract under which the program(s) have been supplied.
+ * This copyright notice must not be removed.
+ *
+ * This Software is licenced by the
+ * "General supply and license conditions for software"
+ * which is part of the standard terms and conditions of sale from HBM.
+*/
+
+/** @file */
+
+#ifndef _NETADAPTER_H
+#define _NETADAPTER_H
+
+#include <vector>
+#include <deque>
+#include <string>
+#include <stdint.h>
+
+namespace hbm {
+	enum enResult {
+		ERR_SUCCESS = 0,
+		ERR_NO_SUCCESS = -1,
+		ERR_INVALIDADAPTER = -2,
+		ERR_INVALIDIPADDRESS = -3,
+		ERR_INVALIDCONFIGMETHOD = -4,
+		ERR_ADAPTERISDOWN = -5,
+		WARN_RESTART_REQUIRED = 4,
+		WARN_INVALIDCONFIGMETHOD = 5
+	};
+
+	struct ipv6Address_t {
+		ipv6Address_t()
+			: address()
+			, prefix(0)
+		{
+		}
+
+		bool equal(const struct ipv6Address_t& op) const
+		{
+			if( (address==op.address) && prefix==op.prefix) {
+				return true;
+			}
+			return false;
+		}
+
+		std::string address;
+		unsigned int prefix;
+	};
+
+	struct ipv4Address_t {
+		bool equal(const struct ipv4Address_t& op) const
+		{
+			if( (address==op.address) && (netmask==op.netmask) ) {
+				return true;
+			}
+			return false;
+		}
+		std::string address;
+		std::string netmask;
+	};
+
+
+	class Netadapter
+	{
+	public:
+
+		typedef std::vector < ipv6Address_t > addressesWithPrefix_t;
+		// we use a double ended queue here because we might insert to the front or to the back.
+		typedef std::deque < ipv4Address_t > addressesWithNetmask_t;
+
+		Netadapter();
+
+		std::string getName() const { return m_name; }
+
+		const addressesWithNetmask_t& getIpv4Addresses() const
+		{
+			return m_ipv4Addresses;
+		}
+
+		const addressesWithPrefix_t& getIpv6Addresses() const
+		{
+			return m_ipv6Addresses;
+		}
+
+		/// returns formatted MAC address in HEX punctuated with ":"
+		std::string getMacAddressString() const { return m_macAddress; }
+
+		/// returns 0 for non FireWire adapters
+		uint64_t getFwGuid() const
+		{
+			return m_fwGuid;
+		}
+
+		unsigned int getIndex() const
+		{
+			return m_index;
+		}
+
+		/// \warning not supported under windows (returns false always)
+		bool isFirewireAdapter() const
+		{
+			if (m_fwGuid) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+		static std::string getIpv4DefaultGateway();
+
+		static bool isApipaAddress(const std::string& address);
+		static bool isValidIpV4Address(const std::string& ip);
+
+		std::string m_name;
+
+		addressesWithNetmask_t m_ipv4Addresses;
+		addressesWithPrefix_t m_ipv6Addresses;
+
+		std::string m_macAddress;
+
+		uint64_t m_fwGuid;
+
+		unsigned int m_index; // interface index retrieved by ::GetAdaptersInfo
+	};
+}
+#endif
---
+++ communication/netadapterlist.h
@@ -0,0 +1,69 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: t; c-basic-offset: 4 -*- */
+/*
+ * Copyright (C) 2007 Hottinger Baldwin Messtechnik GmbH
+ * Im Tiefen See 45
+ * 64293 Darmstadt
+ * Germany
+ * http://www.hbm.com
+ * All rights reserved
+ *
+ * The copyright to the computer program(s) herein is the property of
+ * Hottinger Baldwin Messtechnik GmbH (HBM), Germany. The program(s)
+ * may be used and/or copied only with the written permission of HBM
+ * or in accordance with the terms and conditions stipulated in the
+ * agreement/contract under which the program(s) have been supplied.
+ * This copyright notice must not be removed.
+ *
+ * This Software is licenced by the
+ * "General supply and license conditions for software"
+ * which is part of the standard terms and conditions of sale from HBM.
+*/
+
+
+/** @file */
+
+#ifndef _HBM__NETADAPTERLIST_H
+#define _HBM__NETADAPTERLIST_H
+
+#include <vector>
+#include <map>
+#include <string>
+
+#include <boost/thread/mutex.hpp>
+
+#include "netadapter.h"
+
+namespace hbm {
+	/// Informationen ueber alle verfuegbaren IP-Schnittstellen.
+	class NetadapterList
+	{
+	public:
+		/// interface index is the key
+		typedef std::map < unsigned int, Netadapter > tAdapters;
+		typedef std::vector < Netadapter > tAdapterArray;
+
+		NetadapterList();
+
+		tAdapters get() const;
+
+		/// the same order as returned by get()
+		tAdapterArray getArray() const;
+
+		/// \throws hbm::exception
+		Netadapter getAdapterByName(const std::string& adapterName) const;
+
+		/// get adapter by interface index
+		/// \throws hbm::exception
+		Netadapter getAdapterByInterfaceIndex(unsigned int interfaceIndex) const;
+
+		void update();
+
+	private:
+
+		void enumAdapters();
+
+		tAdapters m_adapters;
+		mutable boost::mutex m_adaptersMtx;
+	};
+}
+#endif
