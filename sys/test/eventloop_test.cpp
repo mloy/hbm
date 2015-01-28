caf826bef4dcf853f6884297ec661d50591658e3
Matthias Loy <matthias.loy@hbm.com>
2015-01-28 10:19:39 +0000

    tests
    git-svn-id: http://repository.devel.hbm.com/svn/quantumx/trunk@22874 7cbc33cc-6077-418a-a044-ec03dd7e8bdb

--- sys/test/eventloop_test.cpp
+++ sys/test/eventloop_test.cpp
@@ -37,17 +37,54 @@ static ssize_t eventHandlerPrint()
 }
 
 
-static unsigned int eventsLeft;
 
 /// by returning error, the execute() method, that is doing the eventloop, exits
-static ssize_t eventHandlerStopAfterN()
+static ssize_t eventHandlerStopAfterN(unsigned int* pEventsLeft)
 {
-	--eventsLeft;
-	if (eventsLeft==0) {
+	--(*pEventsLeft);
+	if ((*pEventsLeft)==0) {
 		return -1;
 	} else {
 		return 0;
 	}
+}
+
+
+/// start the eventloop in a separate thread wait some time and stop it.
+BOOST_AUTO_TEST_CASE(stop_test)
+{
+
+	hbm::sys::EventLoop eventLoop;
+
+	static const std::chrono::milliseconds waitDuration(300);
+
+	std::chrono::steady_clock::time_point startTime = std::chrono::steady_clock::now();
+	std::thread worker(std::bind(&hbm::sys::EventLoop::execute, &eventLoop));
+	std::this_thread::sleep_for(waitDuration);
+	eventLoop.stop();
+	worker.join();
+	std::chrono::steady_clock::time_point endTime = std::chrono::steady_clock::now();
+
+	std::chrono::milliseconds delta = std::chrono::duration_cast < std::chrono::milliseconds > (endTime-startTime);
+
+	BOOST_CHECK_MESSAGE(delta.count()>=300, "unexpected wait difference of " << std::to_string(delta.count()) << "ms");
+}
+
+
+BOOST_AUTO_TEST_CASE(waitforend_test)
+{
+	hbm::sys::EventLoop eventLoop;
+
+	static const std::chrono::milliseconds duration(100);
+
+	std::chrono::steady_clock::time_point startTime = std::chrono::steady_clock::now();
+	int result = eventLoop.execute_for(duration);
+	std::chrono::steady_clock::time_point endTime = std::chrono::steady_clock::now();
+
+	std::chrono::milliseconds delta = std::chrono::duration_cast < std::chrono::milliseconds > (endTime-startTime);
+
+	BOOST_CHECK_EQUAL(result, 0);
+	BOOST_CHECK_GE(delta.count(), duration.count()-3);
 }
 
 BOOST_AUTO_TEST_CASE(norify_test)
@@ -66,50 +103,11 @@ BOOST_AUTO_TEST_CASE(norify_test)
 	notifier.notify();
 	result = eventLoop.execute_for(duration);
 	BOOST_CHECK(result==-1);
-
-}
-
-/// start the eventloop in a separate thread wait some time and stop it.
-BOOST_AUTO_TEST_CASE(stop_test)
-{
-
-	hbm::sys::EventLoop eventLoop;
-
-	static const std::chrono::milliseconds waitDuration(300);
-
-	std::chrono::steady_clock::time_point startTime = std::chrono::steady_clock::now();
-	std::thread worker(std::bind(&hbm::sys::EventLoop::execute, &eventLoop));
-	std::this_thread::sleep_for(waitDuration);
-	eventLoop.stop();
-	worker.join();
-	std::chrono::steady_clock::time_point endTime = std::chrono::steady_clock::now();
-
-	std::chrono::milliseconds delta = std::chrono::duration_cast < std::chrono::milliseconds > (endTime-startTime-waitDuration);
-
-	BOOST_CHECK_MESSAGE(delta.count()<50, "unexpected wait difference of " << std::to_string(delta.count()) << "ms");
-}
-
-
-BOOST_AUTO_TEST_CASE(waitforend_test)
-{
-	hbm::sys::EventLoop eventLoop;
-
-	static const std::chrono::milliseconds duration(100);
-
-	std::chrono::steady_clock::time_point startTime = std::chrono::steady_clock::now();
-	int result = eventLoop.execute_for(duration);
-	std::chrono::steady_clock::time_point endTime = std::chrono::steady_clock::now();
-
-	std::chrono::milliseconds delta = std::chrono::duration_cast < std::chrono::milliseconds > (endTime-startTime);
-
-	BOOST_CHECK(result==0);
-	BOOST_CHECK(delta.count()>=100);
 }
 
 BOOST_AUTO_TEST_CASE(timerevent_test)
 {
 	static const unsigned int timerCycle = 100;
-	static const unsigned int timerCount = 10;
 	static const std::chrono::milliseconds duration(timerCycle*3);
 	hbm::sys::EventLoop eventLoop;
 
@@ -127,12 +125,13 @@ BOOST_AUTO_TEST_CASE(severaltimerevents_test)
 	static const std::chrono::milliseconds duration(timerCycle * 3);
 	hbm::sys::EventLoop eventLoop;
 
-	eventsLeft = 2;
+	unsigned int eventsLeft = 2;
 
 	hbm::sys::Timer timer(timerCycle);
-	eventLoop.addEvent(timer.getFd(), &eventHandlerStopAfterN);
+	eventLoop.addEvent(timer.getFd(), std::bind(&eventHandlerStopAfterN, &eventsLeft));
 
 	int result = eventLoop.execute_for(duration);
+	BOOST_CHECK_EQUAL(eventsLeft, 0);
 	BOOST_CHECK(result == -1);
 }
 
