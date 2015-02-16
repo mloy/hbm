a4cd34b62ed5835d38c0f702d65541b561e4c4ca
Matthias Loy <matthias.loy@hbm.com>
2015-02-16 08:40:35 +0000

    replace functions with tests
    git-svn-id: http://repository.devel.hbm.com/svn/quantumx/trunk@23169 7cbc33cc-6077-418a-a044-ec03dd7e8bdb

---
+++ string/test/replacetest.h
@@ -0,0 +1,11 @@
+// Copyright 2014 Hottinger Baldwin Messtechnik
+// Distributed under MIT license
+// See file LICENSE provided
+
+
+#ifndef SPLITTEST_H
+#define SPLITTEST_H
+
+
+#endif // SPLITTEST_H
+
