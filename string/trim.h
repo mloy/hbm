89895c9b209cb589f614749a9d5d1bae940cdfc4
Matthias Loy <matthias.loy@hbm.com>
2015-03-25 12:55:07 +0000

    trim_left_if, trim_right_if
    git-svn-id: http://repository.devel.hbm.com/svn/quantumx/trunk@23857 7cbc33cc-6077-418a-a044-ec03dd7e8bdb

--- string/trim.h
+++ string/trim.h
@@ -17,6 +17,10 @@ namespace hbm {
 		void trim_right(std::string& text);
 
 		void trim(std::string& text);
+
+		void trim_left_if(std::string& text, char ch);
+
+		void trim_right_if(std::string& text, char ch);
 	}
 }
 #endif // __HBM__STRING__TRIM_H
