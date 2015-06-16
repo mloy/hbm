c879d52d6926c8a1943e0ea2cca0ff9ed5dee918
Matthias Loy <matthias.loy@hbm.com>
2015-06-16 12:13:02 +0000

    adapt to changes
    git-svn-id: http://repository.devel.hbm.com/svn/quantumx/trunk@25065 7cbc33cc-6077-418a-a044-ec03dd7e8bdb

--- communication/windows/netlink.cpp
+++ communication/windows/netlink.cpp
@@ -13,57 +13,59 @@
 
 
 namespace hbm {
-	Netlink::Netlink(communication::NetadapterList &netadapterlist, sys::EventLoop &eventLoop)
-		: m_netadapterlist(netadapterlist)
-		, m_eventloop(eventLoop)
-	{
-		HANDLE handle = NULL;
-		m_overlap.hEvent = WSACreateEvent();
-		DWORD ret = NotifyAddrChange(&handle, &m_overlap);
-		if (ret!=NO_ERROR) {
-			int error = WSAGetLastError();
-			if (error != WSA_IO_PENDING) {
-				throw hbm::exception::exception("NotifyAddrChange failed: " + WSAGetLastError());
+	namespace communication {
+		Netlink::Netlink(communication::NetadapterList &netadapterlist, sys::EventLoop &eventLoop)
+			: m_netadapterlist(netadapterlist)
+			, m_eventloop(eventLoop)
+		{
+			HANDLE handle = NULL;
+			m_overlap.hEvent = WSACreateEvent();
+			DWORD ret = NotifyAddrChange(&handle, &m_overlap);
+			if (ret != NO_ERROR) {
+				int error = WSAGetLastError();
+				if (error != WSA_IO_PENDING) {
+					throw hbm::exception::exception("NotifyAddrChange failed: " + WSAGetLastError());
+				}
 			}
 		}
-	}
 
-	Netlink::~Netlink()
-	{
-		stop();
-	}
-
-	ssize_t Netlink::process()
-	{
-		m_netadapterlist.update();
-		if (m_eventHandler) {
-			m_eventHandler(COMPLETE, 0, "");
+		Netlink::~Netlink()
+		{
+			stop();
 		}
 
-		HANDLE handle = NULL;
-		DWORD ret = NotifyAddrChange(&handle, &m_overlap);
-		if (ret != NO_ERROR) {
-			int error = WSAGetLastError();
-			if (error != WSA_IO_PENDING) {
-				throw hbm::exception::exception("NotifyAddrChange failed: Error " + WSAGetLastError());
+		ssize_t Netlink::process()
+		{
+			m_netadapterlist.update();
+			if (m_interfaceAddressEventHandler) {
+				m_interfaceAddressEventHandler(COMPLETE, 0, "");
 			}
-		}
-		return 0;
-	}
 
-	int Netlink::start(cb_t eventHandler)
-	{
-		m_eventHandler = eventHandler;
-		if (m_eventHandler) {
-			m_eventHandler(COMPLETE, 0, "");
+			HANDLE handle = NULL;
+			DWORD ret = NotifyAddrChange(&handle, &m_overlap);
+			if (ret != NO_ERROR) {
+				int error = WSAGetLastError();
+				if (error != WSA_IO_PENDING) {
+					throw hbm::exception::exception("NotifyAddrChange failed: Error " + WSAGetLastError());
+				}
+			}
+			return 0;
 		}
-		m_eventloop.addEvent(m_overlap.hEvent, std::bind(&Netlink::process, this));
-		return 0;
-	}
 
-	int Netlink::stop()
-	{
-		m_eventloop.eraseEvent(m_overlap.hEvent);
-		return CloseHandle(m_overlap.hEvent);
+		int Netlink::start(interfaceAddressCb_t interfaceAddressEventHandler, defaultGatewayCb_t defaultGatewayEventHandler)
+		{
+			m_interfaceAddressEventHandler = interfaceAddressEventHandler;
+			if (m_interfaceAddressEventHandler) {
+				m_interfaceAddressEventHandler(COMPLETE, 0, "");
+			}
+			m_eventloop.addEvent(m_overlap.hEvent, std::bind(&Netlink::process, this));
+			return 0;
+		}
+
+		int Netlink::stop()
+		{
+			m_eventloop.eraseEvent(m_overlap.hEvent);
+			return CloseHandle(m_overlap.hEvent);
+		}
 	}
 }
