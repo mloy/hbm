f3e1794a0ce495883df0a62ccac480e86d945393
Matthias Loy <matthias.loy@hbm.com>
2016-03-17 16:01:38 +0100

    tool: Error handling

--- communication/tool/Client.cpp
+++ communication/tool/Client.cpp
@@ -20,16 +20,34 @@ int main(int argc, char* argv[])
 	std::string address = argv[1];
 	std::string port = argv[2];
 
-	result = client.connect(argv[1], argv[2]);
+	result = client.connect(address, port);
 	if (result!=0) {
+		std::cerr << "could not connect to server!" << std::endl;
 		return EXIT_FAILURE;
 	}
 	const char sndBuffer[] = "hallo";
 	char recvBuffer[1024];
 
+	result = client.receiveComplete(recvBuffer, sizeof(sndBuffer));
+
 	do {
 		result = client.sendBlock(sndBuffer, sizeof(sndBuffer), false);
+		if (result != sizeof(sndBuffer)) {
+			std::cerr << "could not send complete data! " << result << " of " << sizeof(sndBuffer) << " bytes send" << std::endl;
+			return EXIT_FAILURE;
+		}
+
 		result = client.receiveComplete(recvBuffer, sizeof(sndBuffer));
+		if (result != sizeof(sndBuffer)) {
+			std::cerr << "received unexpected amount of data! " << result << " of " << sizeof(sndBuffer) << " bytes received " << std::endl;
+			return EXIT_FAILURE;
+		}
+		if (strncmp(recvBuffer, sndBuffer, sizeof(sndBuffer))) {
+			std::cerr << "received unexpected data! " << recvBuffer << "!=" << sndBuffer << std::endl;
+		}
+
+
+
 	} while (true);
 
 	return EXIT_SUCCESS;
