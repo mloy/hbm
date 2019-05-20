7cd0921109f147d7a74d16865470487b9a525d5f
Matthias Loy <matthias.loy@hbm.com>
2019-05-20 20:56:24 +0200

    tool: measure performance

--- tool/sys/eventloopperformance.cpp
+++ tool/sys/eventloopperformance.cpp
@@ -1,4 +1,4 @@
-#include <chrono>
+﻿#include <chrono>
 #include <cstdlib>
 #include <functional>
 #include <iostream>
@@ -22,7 +22,7 @@ static void notifierCb()
 	}
 }
 
-int main()
+static void notify()
 {
 	notifier.set(std::bind(&notifierCb));
 	notifier.notify();
@@ -36,5 +36,25 @@ int main()
 	std::chrono::microseconds diff = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1);
 
 	std::cout << "execution time for " << EVENTLIMIT << " event notifications: " << diff.count() << "µs" << std::endl;
+}
+
+static void addRemove()
+{
+	std::chrono::high_resolution_clock::time_point t1;
+	std::chrono::high_resolution_clock::time_point t2;
+	t1 = std::chrono::high_resolution_clock::now();
+	for (size_t count = 0; count<EVENTLIMIT; ++count) {
+		// By creating the notifier it is added to the eventloop. It is removed on destruction.
+		hbm::sys::Notifier tempNotifier(eventloop);
+	}
+	t2 = std::chrono::high_resolution_clock::now();
+	std::chrono::microseconds diff = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1);
+	std::cout << "execution time for create/destruct " << EVENTLIMIT << " notifiers: " << diff.count() << "µs" << std::endl;
+}
+
+int main()
+{
+	notify();
+	addRemove();
 	return EXIT_SUCCESS;
 }
